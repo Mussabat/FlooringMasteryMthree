21c62d25ef54b9d1be6b6bd7e4435598eb880591
Mussabat <tnafisa18@gmail.com>
2026-09-19 22:21:09 +0100

    chore: format the file userio

--- src/main/java/com/flooring/mastery/view/UserIO.java
+++ src/main/java/com/flooring/mastery/view/UserIO.java
@@ -1,19 +1,19 @@
 package com.flooring.mastery.view;
 
-    public interface UserIO {
-        // shows a message on screen
-        void print(String message);
+public interface UserIO {
+    // shows a message on screen
+    void print(String message);
 
-        // shows the prompt and returns whatever text the user types
-        String readString(String prompt);
+    // shows the prompt and returns whatever text the user types
+    String readString(String prompt);
 
-        // shows the prompt and returns the whole number
+    // shows the prompt and returns the whole number
 
-        int readInt(String prompt);
+    int readInt(String prompt);
 
-        // shows the prompt and returns the whole number between min and max
-        int readInt(String prompt, int min, int max);
+    // shows the prompt and returns the whole number between min and max
+    int readInt(String prompt, int min, int max);
 
 
-    }
+}
 
