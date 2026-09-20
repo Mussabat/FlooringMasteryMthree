83edcf1e497b26e4f96a07bac097361ea9e4ecb1
Mussabat <tnafisa18@gmail.com>
2026-09-20 10:01:50 +0100

    add: App main

--- src/main/java/com/flooring/mastery/App.java
+++ src/main/java/com/flooring/mastery/App.java
@@ -1,4 +1,19 @@
 package com.flooring.mastery;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.flooring.mastery.controller.FlooringController;
+
 public class App {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.scan("com.flooring.mastery");
+        appContext.refresh();
+        FlooringController controller = appContext.getBean("flooringController", FlooringController.class);
+        controller.run();
+        appContext.close();
+    }
 }
+
+
